4eb9c095d3210744e6b9ebb8626cfee07ebea811
Fernando Paredes Garcia <fernando@develcuy.com>
2016-11-19 03:06:00 -0500

    Add 'google' namespace to corresponding codebase elements and database tables.

--- README.md
+++ README.md
@@ -1,27 +1,30 @@
 
 
-### Installation
+## Installation
+
+### Google
+
 Run the following SQL queries in strict order:
 
 ```SQL
 -- SQLite3
-CREATE TABLE oauth2_nonce(id CHAR(36) PRIMARY KEY, created UNSIGNED BIG INT);
-CREATE TABLE oauth2_token(id VARCHAR(255) PRIMARY KEY, access_token VARCHAR(255), refresh_token VARCHAR(255), resource VARCHAR(255), user_id UNSIGNED BIG INT, created UNSIGNED BIG INT, changed UNSIGNED BIG INT, ttl UNSIGNED BIG INT);
-CREATE UNIQUE INDEX uidx_oauth2_token_res ON oauth2_token(resource);
-CREATE INDEX idx_oauth2_token_user_id ON oauth2_token(user_id);
-CREATE INDEX idx_oauth2_token_rucct ON oauth2_token(resource, user_id, created, changed, ttl);
+CREATE TABLE oauth2_google_nonce(id CHAR(36) PRIMARY KEY, created UNSIGNED BIG INT);
+CREATE TABLE oauth2_google_token(id VARCHAR(255) PRIMARY KEY, access_token VARCHAR(255), refresh_token VARCHAR(255), resource VARCHAR(255), user_id UNSIGNED BIG INT, created UNSIGNED BIG INT, changed UNSIGNED BIG INT, ttl UNSIGNED BIG INT);
+CREATE UNIQUE INDEX uidx_oauth2_google_token_ru ON oauth2_google_token(resource, user_id);
+CREATE INDEX idx_oauth2_google_token_user_id ON oauth2_google_token(user_id);
+CREATE INDEX idx_oauth2_google_token_rucct ON oauth2_google_token(resource, user_id, created, changed, ttl);
 ```
 
 ```SQL
 -- PostgreSQL
-CREATE TABLE oauth2_nonce(id char (36) PRIMARY KEY NOT NULL, created bigint NOT NULL);
-CREATE TABLE oauth2_token(id varchar(255) PRIMARY KEY NOT NULL, access_token varchar(255), refresh_token varchar(255), resource varchar(255), user_id BIGINT NOT NULL, created BIGINT NOT NULL, changed BIGINT NOT NULL, ttl BIGINT NOT NULL);
-CREATE UNIQUE INDEX uidx_oauth2_token_ru ON oauth2_token USING btree (resource COLLATE pg_catalog."default", user_id);
-CREATE INDEX idx_oauth2_token_user_id ON oauth2_token USING btree (user_id);
-CREATE INDEX idx_oauth2_token_rucct ON oauth2_token USING btree (resource COLLATE pg_catalog."default", user_id, created, changed, ttl);
+CREATE TABLE oauth2_google_nonce(id char (36) PRIMARY KEY NOT NULL, created bigint NOT NULL);
+CREATE TABLE oauth2_google_token(id varchar(255) PRIMARY KEY NOT NULL, access_token varchar(255), refresh_token varchar(255), resource varchar(255), user_id BIGINT NOT NULL, created BIGINT NOT NULL, changed BIGINT NOT NULL, ttl BIGINT NOT NULL);
+CREATE UNIQUE INDEX uidx_oauth2_google_token_ru ON oauth2_google_token USING btree (resource COLLATE pg_catalog."default", user_id);
+CREATE INDEX idx_oauth2_google_token_user_id ON oauth2_google_token USING btree (user_id);
+CREATE INDEX idx_oauth2_google_token_rucct ON oauth2_google_token USING btree (resource COLLATE pg_catalog."default", user_id, created, changed, ttl);
 ```
 
-### Configuration
+## Configuration
 
 Open your app in Google Developer Console(https://console.developers.google.com),
 click APIs & auth, click Credentials, then 'Create new Client ID', pick 'Web
@@ -32,7 +35,9 @@ Add the following to settings.lua:
 ```Lua
 settings.oauth2 = {
   nonce_ttl = 2*60, -- default: 2 minutes
-  client_id = [client_id],
-  client_secret = [client_secret],
+  google = {
+    client_id = [g_client_id],
+    client_secret = [g_client_secret],
+  },
 }
 ```
--- init.lua
+++ init.lua
@@ -5,51 +5,61 @@ ophal.modules.oauth2 = _M
 
 local seawolf = require 'seawolf'.__build('contrib', 'text')
 local config = settings.oauth2
-local xtable, time = seawolf.contrib.seawolf_table, os.time
+local xtable, time, base = seawolf.contrib.seawolf_table, os.time, base
 local explode, env, tonumber = seawolf.text.explode, env, tonumber
 local empty, goto, site = seawolf.variable.empty, goto, settings.site
-local db_query
+local header, route_arg, sleep = header, route_arg, socket.sleep
+local request_uri, escape = request_uri, socket.url.escape
+local _SESSION = _SESSION
+local db_query, user_mod
 
 function _M.init()
   db_query = env.db_query
+  user_mod = ophal.modules.user
+
+  -- Oauth2 session fingerprint
+  if _SESSION and _SESSION.oauth2 == nil then
+    _SESSION.oauth2 = {}
+  end
 end
 
 function _M.cron()
-  db_query('DELETE FROM oauth2_nonce WHERE ? > created + ?', time(), config.nonce_ttl or 2*60)
+  db_query('DELETE FROM oauth2_google_nonce WHERE ? > created + ?', time(), config.google.nonce_ttl or 2*60)
 end
 
 function _M.route()
   local items = {}
 
-  items['oauth2/callback'] = {
-    page_callback = 'callback',
+  items['oauth2/google'] = {
+    page_callback = 'google_callback',
+    format = 'json',
   }
 
   return items
 end
 
-function _M.valid_nonce(nonce)
-  local rs = db_query('SELECT COUNT(*) total FROM oauth2_nonce WHERE id = ? AND ? <= created + ?', nonce, time(), config.nonce_ttl or 2*60)
+function _M.google_valid_nonce(nonce)
+  local rs = db_query('SELECT COUNT(*) total FROM oauth2_google_nonce WHERE id = ? AND ? <= created + ?', nonce, time(), config.google.nonce_ttl or 2*60)
   local count = rs:fetch(true)
   if count then
     return tonumber(count.total) > 0
   end
 end
 
-function _M.create_authcodes(entity)
-  local rs = db_query('INSERT INTO oauth2_token(id, resource, access_token, refresh_token, user_id, created, changed, ttl) VALUES(?, ?, ?, ?, ?, ?, ?, ?)', entity.id, entity.resource, entity.access_token, entity.refresh_token, entity.user_id, time(), time(), entity.ttl)
+function _M.google_create_authcodes(entity)
+  local rs = db_query('INSERT INTO oauth2_google_token(id, resource, access_token, refresh_token, user_id, created, changed, ttl) VALUES(?, ?, ?, ?, ?, ?, ?, ?)', entity.id, entity.resource, entity.access_token, entity.refresh_token, entity.user_id, time(), time(), entity.ttl)
 end
 
-function _M.load_authcodes(id)
-  local rs = db_query('SELECT * FROM oauth2_token WHERE id = ?', id)
+function _M.google_load_authcodes(id)
+  local rs = db_query('SELECT * FROM oauth2_google_token WHERE id = ?', id)
   return rs:fetch(true)
 end
 
-function _M.update_authcodes(entity)
-  local rs = db_query('UPDATE oauth2_token SET access_token = ?, changed = ?, ttl = ? WHERE id = ?', entity.access_token, time(), entity.ttl, entity.id)
+function _M.google_update_authcodes(entity)
+  local rs = db_query('UPDATE oauth2_google_token SET access_token = ?, changed = ?, ttl = ? WHERE id = ?', entity.access_token, time(), entity.ttl, entity.id)
 end
 
-function _M.get_resource(id)
+function _M.google_get_resource(id)
   if empty(_M.resources) then
     _M.resources = module_invoke_all 'oauth2_resource'
   end
@@ -59,14 +69,14 @@ function _M.get_resource(id)
   end
 end
 
-function _M.get_authcodes(code)
+function _M.google_get_authcodes(code)
   local json = require 'dkjson'
   local https = require 'ssl.https'
   local ltn12 = require 'ltn12'
   local body = _M.build_params{
     code = code,
-    client_id = config.client_id,
-    client_secret = config.client_secret,
+    client_id = config.google.client_id,
+    client_secret = config.google.client_secret,
     redirect_uri = ('%s://%s/oauth2/callback'):format(site.scheme or 'http', _SERVER 'SERVER_NAME'),
     grant_type = 'authorization_code',
   }
@@ -89,20 +99,20 @@ function _M.get_authcodes(code)
   end
 end
 
-function _M.callback()
+function _M.google_callback()
   local state = explode('|', _GET.state)
   local resource_id = state[1]
   local nonce = state[2]
 
-  local resource = _M.get_resource(resource_id)
+  local resource = _M.google_get_resource(resource_id)
 
-  if _M.valid_nonce(nonce) and not empty(resource) then
-    local res = _M.get_authcodes(_GET.code)
+  if _M.google_valid_nonce(nonce) and not empty(resource) then
+    local res = _M.google_get_authcodes(_GET.code)
     res.id = _GET.code
     res.resource = resource_id
     res.user_id = 1
     res.ttl = res.expires_in
-    _M.create_authcodes(res)
+    _M.google_create_authcodes(res)
   end
 
   goto(resource.path)
@@ -111,31 +121,30 @@ function _M.callback()
 end
 
 function _M.build_params(params)
-  local url = require 'socket.url'
   local output = xtable()
   for k, v in pairs(params) do
-    output:append(k .. '=' .. url.escape(v))
+    output:append(k .. '=' .. escape(v))
   end
   return output:concat '&'
 end
 
-function _M.get_nonce(resource)
+function _M.google_get_nonce(resource)
   local uuid = require 'uuid'
   local nonce = uuid.new()
-  local rs, err = db_query('INSERT INTO oauth2_nonce(id, created) VALUES(?, ?)', nonce, time())
+  local rs, err = db_query('INSERT INTO oauth2_google_nonce(id, created) VALUES(?, ?)', nonce, time())
   return resource .. '|' .. nonce
 end
 
-function _M.refresh_authcodes(id)
-  local token = _M.load_authcodes(id)
+function _M.google_refresh_authcodes(id)
+  local token = _M.google_load_authcodes(id)
   if not empty(token) then
     local json = require 'dkjson'
     local https = require 'ssl.https'
     local ltn12 = require 'ltn12'
     local body = _M.build_params{
       refresh_token = token.refresh_token,
-      client_id = config.client_id,
-      client_secret = config.client_secret,
+      client_id = config.google.client_id,
+      client_secret = config.google.client_secret,
       grant_type = 'refresh_token',
     }
     local response = {}
@@ -157,34 +166,34 @@ function _M.refresh_authcodes(id)
   end
 end
 
-function _M.get_token(resource, user_id)
-  local rs, err = db_query('SELECT id, access_token, refresh_token, changed, ttl FROM oauth2_token WHERE resource = ? AND user_id = ? ORDER BY changed DESC', resource, user_id)
+function _M.google_get_token(resource, user_id)
+  local rs, err = db_query('SELECT id, access_token, refresh_token, changed, ttl FROM oauth2_google_token WHERE resource = ? AND user_id = ? ORDER BY changed DESC', resource, user_id)
   local token = rs:fetch(true)
   if not empty(token) then
     if token.changed + token.ttl > time() then
       return token.access_token
     else
-      res = _M.refresh_authcodes(token.id)
+      res = _M.google_refresh_authcodes(token.id)
       if res then
         res.id = token.id
         res.ttl = res.expires_in
-        _M.update_authcodes(res)
+        _M.google_update_authcodes(res)
         return res.access_token
       end
     end
   end
 end
 
-function theme.oauth2_connect(variables)
+function theme.oauth2_google_connect(variables)
   local variables = variables or {}
 
   local base_url = 'https://accounts.google.com/o/oauth2'
   local params = {
     scope = 'https://www.googleapis.com/auth/analytics.readonly',
-    state = _M.get_nonce(variables.resource),
+    state = _M.google_get_nonce(variables.resource),
     redirect_uri = ('%s://%s/oauth2/callback'):format(site.scheme or 'http', _SERVER 'SERVER_NAME'),
     response_type = 'code',
-    client_id = config.client_id,
+    client_id = config.google.client_id,
     access_type = 'offline',
     approval_prompt = 'force',
   }
